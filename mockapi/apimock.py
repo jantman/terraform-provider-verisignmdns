e8b4e7e9ee580433e5aca7b85addad34185e9c8c
Jason Antman <jason@jasonantman.com>
2019-02-08 11:39:12 -0500

    finish apimock.py

--- mockapi/apimock.py
+++ mockapi/apimock.py
@@ -77,21 +77,196 @@ def abort_if_bad_headers():
 class ResourceRecord(Resource):
 
     def get(self, accountId, zoneName, resourceRecordId):
+        """
+        GET /api/v1/accounts/{accountId}/zones/{zoneName}/rr/{resourceRecordId}
+        Accept: application/json
+        Content-Type: application/json
+
+        200
+        {
+          "resource_record_id": "1234",
+          "owner": "www.example.com",
+          "type": "A",
+          "rdata": "192.0.2.29",
+          "ttl": "86400",
+          "create_timestamp": "2011-01-21T16:35:35.000+00:00",
+          "update_timestamp": "2011-01-21T16:35:35.000+00:00",
+          "created_by": "someuser",
+          "updated_by": "someotheruser"
+        }
+
+        400 - The request was malformed. Please check the format of the request.
+
+        422 - The request was well-formed, but one or more validations failed.
+        {
+          "error_code": "ERROR_OPERATION_FAILURE",
+          "error_messages": [
+            "Domain already exists. Please verify your domain name."
+          ]
+        }
+        """
         abort_if_bad_auth()
         abort_if_bad_headers()
-        app.logger.info(vars(request))
-        return {'hello': 'world'}
+        abort_if_bad_account_or_zone(accountId, zoneName)
+        if resourceRecordId not in RECORDS:
+            app.logger.info('DELETE - 422 - bad ID')
+            abort(
+                422,
+                error_code='BAD_ID', error_messages=['record ID invalid']
+            )
+        return RECORDS[resourceRecordId], 200
 
     def put(self, accountId, zoneName, resourceRecordId):
+        """
+        PUT /api/v1/accounts/{accountId}/zones/{zoneName}/rr/{resourceRecordId}
+        Accept: application/json
+        Content-Type: application/json
+
+        Request:
+        {
+          "rdata": "192.0.2.30",
+          "comments": "Updating a resource record."
+        }
+
+        200 Resource record successfully updated.
+        Headers:
+        Location - URL of created resource record with its resource record Id
+
+        400 - The request was malformed. Please check the format of the request.
+
+        422 - The request was well-formed, but one or more validations failed.
+        {
+          "error_code": "ERROR_OPERATION_FAILURE",
+          "error_messages": [
+            "Domain already exists. Please verify your domain name."
+          ]
+        }
+        """
         abort_if_bad_auth()
         abort_if_bad_headers()
-        RECORDS[resourceRecordId] = request.form['data']
-        return {}, 201, {'X-Header': 'value'}
+        abort_if_bad_account_or_zone(accountId, zoneName)
+        args = parser.parse_args(strict=True)
+        app.logger.info('PUT: %s' % args)
+        if not set(args.keys).issubset(set(['rdata', 'comments'])):
+            app.logger.info('PUT - 422 - bad fields')
+            abort(
+                422,
+                error_code='BAD_FIELDS', error_messages=['foo']
+            )
+        if resourceRecordId not in RECORDS:
+            app.logger.info('PUT - 422 - bad ID')
+            abort(
+                422,
+                error_code='BAD_ID', error_messages=['record ID invalid']
+            )
+        if 'rdata' in args:
+            RECORDS[resourceRecordId]['rdata'] = args['rdata']
+        if 'comments' in args:
+            RECORDS[resourceRecordId]['comments'] = args['comments']
+        loc = 'http://%s/api/v1/accounts/%s/zones/%s/rr/%d' % (
+            request.environ['HTTP_HOST'], ACCOUNT_ID, ZONE_NAME, k
+        )
+        return '', 200, {'Location': loc}
+
+    def delete(self, accountId, zoneName, resourceRecordId):
+        """
+        DELETE /api/v1/accounts/{accountId}/zones/{zoneName}/rr/{resourceRecordId}
+        Accept: application/json
+        Content-Type: application/json
+
+        Request:
+        {
+          "comments": "Deleting example.com zone"
+        }
+
+        204 - Successfully deleted
+
+        400 - The request was malformed. Please check the format of the request.
+
+        422 - The request was well-formed, but one or more validations failed.
+        {
+          "error_code": "ERROR_OPERATION_FAILURE",
+          "error_messages": [
+            "Domain already exists. Please verify your domain name."
+          ]
+        }
+        """
+        abort_if_bad_auth()
+        abort_if_bad_headers()
+        abort_if_bad_account_or_zone(accountId, zoneName)
+        args = parser.parse_args(strict=True)
+        app.logger.info('DELETE: %s' % args)
+        if len(args.keys) > 0 and list(args.keys) != ['comments']:
+            app.logger.info('DELETE - 422 - bad fields')
+            abort(
+                422,
+                error_code='BAD_FIELDS', error_messages=['foo']
+            )
+        if resourceRecordId not in RECORDS:
+            app.logger.info('DELETE - 422 - bad ID')
+            abort(
+                422,
+                error_code='BAD_ID', error_messages=['record ID invalid']
+            )
+        del RECORDS[resourceRecordId]
+        return '', 204
 
 
 class ResourceRecordList(Resource):
 
     def get(self, accountId, zoneName):
+        """
+        GET /api/v1/accounts/{accountId}/zones/{zoneName}/rr
+        Accept: application/json
+        Content-Type: application/json
+
+        API is paginated, but this mock doesn't expose that, as the provider
+        doesn't use this endpoint
+
+        200 OK:
+        {
+          "total_count": 2,
+          "zone_name": "example.com",
+          "zone_id": 12345,
+          "view_name": "ASIA",
+          "resource_records":[
+                {
+                  "resource_record_id": "1234",
+                  "modifiable": true,
+                  "owner": "www.example.com",
+                  "type": "A",
+                  "rdata": "192.0.2.25",
+                  "ttl": "86400",
+                  "create_timestamp": "2011-01-21T16:35:35.000+00:00",
+                  "update_timestamp": "2011-01-21T16:35:35.000+00:00",
+                  "created_by": "someuser",
+                  "updated_by": "someotheruser"
+                },
+                {
+                  "resource_record_id": "1235",
+                  "modifiable": true,
+                  "owner": "www1.example.com",
+                  "type": "A",
+                  "rdata": "192.0.2.26",
+                  "ttl": "86400",
+                  "create_timestamp": "2011-01-21T16:35:35.000+00:00",
+                  "update_timestamp": "2011-01-21T16:35:35.000+00:00",
+                  "created_by": "someuser",
+                  "updated_by": "someotheruser"
+                }
+            ]
+        }
+
+        400 - The request was malformed. Please check the format of the request.
+
+        422 - The request was well-formed, but one or more validations failed.
+        {
+          "error_code": "ERROR_OPERATION_FAILURE",
+          "error_messages": [
+            "Domain already exists. Please verify your domain name."
+          ]
+        }
+        """
         abort_if_bad_auth()
         abort_if_bad_headers()
         abort_if_bad_account_or_zone(accountId, zoneName)
@@ -104,6 +279,33 @@ class ResourceRecordList(Resource):
         }, 200
 
     def post(self, accountId, zoneName):
+        """
+        POST /api/v1/accounts/{accountId}/zones/{zoneName}/rr
+        Accept: application/json
+        Content-Type: application/json
+
+        Request:
+        {
+          "owner": "www.example.com",
+          "type": "A",
+          "rdata": "192.0.2.27",
+          "comments": "Adding a resource record"
+        }
+
+        201 Resource record successfully created.
+        Headers:
+        Location - URL of created resource record with its resource record Id
+
+        400 - The request was malformed. Please check the format of the request.
+
+        422 - The request was well-formed, but one or more validations failed.
+        {
+          "error_code": "ERROR_OPERATION_FAILURE",
+          "error_messages": [
+            "Domain already exists. Please verify your domain name."
+          ]
+        }
+        """
         abort_if_bad_auth()
         abort_if_bad_headers()
         abort_if_bad_account_or_zone(accountId, zoneName)
